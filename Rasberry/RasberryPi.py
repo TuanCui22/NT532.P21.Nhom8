5f8aa4df5c38b4aaab5e93adc8e025f7bae6228e
ThanhTuan23 <124656776+TuanCui22@users.noreply.github.com>
2025-06-03 15:31:34 +0700

    Update and rename main.py to RasberryPi.py

---
+++ Rasberry/RasberryPi.py
@@ -0,0 +1,32 @@
+# pi_capture_client.py
+import cv2
+import requests
+import time
+
+SERVER_URL = "http://0.0.0.0:5000/upload"  # thay IP server
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("❌ Không mở được camera.")
+    exit()
+
+print("📸 Đang capture, nhấn Ctrl+C để dừng.")
+
+try:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            continue
+
+        _, img_encoded = cv2.imencode('.jpg', frame)
+        response = requests.post(SERVER_URL, files={"image": img_encoded.tobytes()})
+
+        if response.status_code == 200:
+            print("✅ Gửi ảnh thành công.")
+        else:
+            print(f"❌ Gửi ảnh lỗi: {response.status_code}")
+
+except KeyboardInterrupt:
+    print("🛑 Dừng lại.")
+finally:
+    cap.release()
